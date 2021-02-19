08c1c884b00502e4857b4d419ab1451e3f4bec35
crash963 <t-pokorny@seznam.cz>
2021-02-19 12:12:17 +0100

    finished index function

---
+++ app/Http/Controllers/PetsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Owner;
+
+class PetsController extends Controller
+{
+    public function index()
+    {
+        $owners = Owner::orderBy("first_name")->get();
+
+        return view("clinic/index", compact($owners));
+    }
+}
