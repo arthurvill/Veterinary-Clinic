e13a6aa7faa2e5be5393a1444b93d3ac05142393
Teo15sk <teo15sk@gmail.com>
2021-02-19 13:24:40 +0100

    small changes in web.php and create.blade.php for Owner

--- resources/views/owners/create.blade.php
+++ resources/views/owners/create.blade.php
@@ -1,1 +1,7 @@
 <h2>Create a new pet owner</h2>
+
+<form action="{{ action('OwnersController@store') }}" method="post">
+
+    <input type="text" name="first_name" value="{{ old('first_name', $owner->first_name)}}">
+
+</form>
