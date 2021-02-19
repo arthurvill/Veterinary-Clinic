1fff149af639133d0eef98fe27fc7ff14e4c4e69
Teo15sk <teo15sk@gmail.com>
2021-02-19 12:26:32 +0100

    Pet show.blade

---
+++ resources/views/pets/show.blade.php
@@ -0,0 +1,12 @@
+<h1>Pet details</h1>
+
+<h3>Name: {{ $pet->name }}</h3>
+<p>Species: {{ $pet->specie->name }}</p>
+<p>Breed: {{ $pet->breed }}</p>
+<p>Age: {{ $pet->age }}</p>
+<p>Weight: {{ $pet->weight }} lbs</p>
+<h3>Photo of {{ $pet->name }}</h3>
+<img src="{{ $pet->photo }}" alt="{{ $pet->name }}">
+<br>
+<h2>Owner's details</h2>
+<p>Name:</p>
