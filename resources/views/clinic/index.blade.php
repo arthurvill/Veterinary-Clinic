a4e695276584ce7c19c495b44fdb6c319b42e350
Tomas Nejedly <t.nejedly1@gmail.com>
2021-02-19 12:24:58 +0100

    index - display owners and their pets

--- resources/views/clinic/index.blade.php
+++ resources/views/clinic/index.blade.php
@@ -4,11 +4,14 @@
 <ul>
 
 @foreach ($owners as $owner)
-  <h3>owners name:{{ $owner->first_name . $owner->surname }}</h3>
+  <h3>Owner:{{" " . $owner->first_name . " " . $owner->surname }}</h3>
+ <ul>
   @foreach ($owner->pets as $pet)
-    {{  }}
+
+  <li> {{" " . $pet->name}}</li>
       
   @endforeach
+</ul>
 @endforeach
 
 </ul>
