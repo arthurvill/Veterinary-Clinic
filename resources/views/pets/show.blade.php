cdef1a3d3021c077e18c59ca2f7c42d750d0a231
crash963 <t-pokorny@seznam.cz>
2021-02-19 12:28:19 +0100

    corrected img source

--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -6,7 +6,7 @@
 <p>Age: {{ $pet->age }}</p>
 <p>Weight: {{ $pet->weight }} lbs</p>
 <h3>Photo of {{ $pet->name }}</h3>
-<img src="{{ $pet->photo }}" alt="{{ $pet->name }}">
+<img src="../{{ $pet->photo }}" alt="{{ $pet->name }}">
 <br>
 <h2>Owner's details</h2>
 <p>Name:</p>
