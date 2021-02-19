989e90f43840a4b327813a43361edaf1cd537704
Teo15sk <teo15sk@gmail.com>
2021-02-19 13:35:26 +0100

    create.blade.php for Owner finished

--- resources/views/owners/create.blade.php
+++ resources/views/owners/create.blade.php
@@ -1,7 +1,22 @@
 <h2>Create a new pet owner</h2>
 
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <form action="{{ action('OwnersController@store') }}" method="post">
-
+    @csrf
+    <label for="">First name: </label>
     <input type="text" name="first_name" value="{{ old('first_name', $owner->first_name)}}">
-
+    <br><br>
+    <label for="">Surname: </label>
+    <input type="text" name="surname" value="{{ old('surname', $owner->surname)}}">
+    <br><br>
+    <button>Submit</button>
 </form>
